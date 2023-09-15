08794877b09e455b95410b47c3bdb6b4b332ff1d
Farruh Sheripov <sheripov.farruh@gmail.com>
2023-09-15 21:57:03 +0200

    MP-432 Added improvements and provided support for older versions of Django, Python, and Graphene.

--- django_google_structured_logger/formatter.py
+++ django_google_structured_logger/formatter.py
@@ -1,4 +1,6 @@
-from pythonjsonlogger import jsonlogger  # type: ignore
+from typing import Dict, Optional
+
+from pythonjsonlogger import jsonlogger
 
 from .storages import RequestStorage, get_current_request
 
@@ -8,8 +10,9 @@ class GoogleFormatter(jsonlogger.JsonFormatter):
     google_operation_field = "logging.googleapis.com/operation"
     google_labels_field = "logging.googleapis.com/labels"
 
-    def add_fields(self, log_record: dict, record, message_dict: dict):
-        """Set Google default fields
+    def add_fields(self, log_record: Dict, record, message_dict: Dict):
+        """
+        Set Google default fields.
 
         List of Google supported fields:
         https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry
@@ -24,34 +27,53 @@ class GoogleFormatter(jsonlogger.JsonFormatter):
          - sourceLocation
         """
         super().add_fields(log_record, record, message_dict)
-        current_request: RequestStorage | None = get_current_request()
+
+        current_request: Optional[RequestStorage] = get_current_request()
 
         log_record["severity"] = record.levelname
 
-        log_record[self.google_labels_field] = {
-            "request_user_id": current_request.user_id() if current_request else None,
-            "request_user_email": current_request.user_email()
-            if current_request
-            else None,
-            **log_record.pop(self.google_labels_field, {}),
+        # Update each specialized field
+        self._set_labels(log_record, current_request)
+        self._set_operation(log_record, current_request)
+        self._set_source_location(log_record, record)
+
+    def _set_labels(self, log_record: Dict, current_request: Optional[RequestStorage]):
+        """Set the Google labels in the log record."""
+        labels = {
+            "user_id": getattr(current_request, "user_id", None),
+            "user_display_field": getattr(current_request, "user_display_field", None),
+            **log_record.get(self.google_labels_field, {}),
             **log_record.pop("labels", {}),
         }
-        self.stringify_values(log_record[self.google_labels_field])
+        self.stringify_values(labels)
+        log_record[self.google_labels_field] = labels
 
-        log_record[self.google_operation_field] = {
-            "id": current_request.uuid if current_request else None,
-            "last": log_record.get("last_operation", False),
-            **log_record.pop(self.google_operation_field, {}),
+    def _set_operation(
+        self, log_record: Dict, current_request: Optional[RequestStorage]
+    ):
+        """Set the Google operation details in the log record."""
+        operation = {
+            "id": getattr(current_request, "uuid", None),
+            **{
+                k: v
+                for k, v in log_record.items()
+                if k in ["first_operation", "last_operation"] and v
+            },
+            **log_record.get(self.google_operation_field, {}),
             **log_record.pop("operation", {}),
         }
+        log_record[self.google_operation_field] = operation
+
+    def _set_source_location(self, log_record: Dict, record):
+        """Set the Google source location in the log record."""
         log_record[self.google_source_location_field] = {
             "file": record.pathname,
             "line": record.lineno,
             "function": record.funcName,
-            "logger": record.name,
+            "logger_name": record.name,
         }
 
     @staticmethod
-    def stringify_values(dict_to_convert: dict):
+    def stringify_values(dict_to_convert: Dict):
         for key in dict_to_convert:
             dict_to_convert[key] = str(dict_to_convert[key])
